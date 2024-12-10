81aa75eb9212070b8f8e4a976658e946290d7464
Abhi <abhilashvishwa12@gmail.com>
2024-12-10 15:35:38 +0530

    Initial commit with code

---
+++ code.cpp
@@ -0,0 +1,334 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int a = 10;
+//     char c = 'b';
+
+//     a = c;
+
+//     cout << a << endl;
+
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int package;
+//     cin >> package;
+
+//     if (package < 20) {
+//          cout << "offer Accepted" << endl;
+//     }else{
+//         cout << "offer rejected" << endl;
+//     }
+//        return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int marks;
+//     cin >> marks;
+
+//     if (marks < 35){
+//         cout << "You are failed" << endl;
+//     }else{
+//         cout << "You are passed" << endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int a, b;
+//     cin >> a >> b;
+
+//     if(a > b){
+//         cout << "A is greater than B" << endl;
+//     }else{
+//         cout << "B is greater than A" << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int age;
+//     cin >> age;
+
+//     if (age < 18){
+//         cout << "You're not eligible to vote"<< endl;
+//     }
+//     else if ( age == 18){
+//         cout << " you're eligible to Vote"<< endl;
+//     }
+//     else{
+//         cout <<"You are also eligible to Vote"<< endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int count = 1;
+//     int n ;
+//     cin >> n;
+
+//     for(int count = 1; count > n; count++){
+//         cout << "hello Mf"<<endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     for(int count = 1; count <=10; count++){
+//         cout <<count*count<<endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int n;
+//     cin >> n;
+//     for(int i = 0; n >= i; i=i+2){
+//         cout <<i<<endl;
+
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int num ;
+//     cin >> num;
+
+//     for(int i = 1; i > n; )
+
+//     if(num % 2 ==0){
+//         cout << num <<endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     // int i ;
+//     for(int i = 100; i <= 200; i++){
+//       cout << i << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     char name;
+//     for(name = 'A'; name <= 'Z';name = name + 1){
+//       cout << name << endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int n = 100;
+//     int i;
+//     for (i = n; i > 1; i = i - 3){
+//       cout << i << endl;
+//     }
+
+//     return 0;
+// // }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int n = 100;
+//     int i;
+//     for (i = n; i > 1; i = i - 3){
+//       cout << i << endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int n, i;
+//     cin >> n;
+
+//     for (i = 1; i <= 10; i=i+1){
+// //       cout << i * n << endl;
+// //     }
+
+
+// //     return 0;
+// // }
+
+// // #include <bits/stdc++.h>
+// // using namespace std;
+
+// // int main() {
+// //   int n, i, pow, num ;
+// //   // cout << "Enter a number: ";
+// //   cin >> n;
+// //   // cout << "Enter a power: ";
+// //   cin >> pow;
+
+// //   num = n;
+// //   for (i = 1; i < pow; i = i + 1){
+// //     num = num * n ;
+// //   }
+// //   cout << num << endl;
+// // }
+    
+// // #include <bits/stdc++.h>
+// // using namespace std;
+
+// // int main() {
+// //     // Your DSA code here
+// //     int n, i , sum=0;
+// //     cin >> n;
+     
+// //     for (i = 1; i <= n; i = i + 1) {
+// //       sum = sum + i;
+// // //     }
+
+// // //     cout << sum ; 
+
+// // // }
+
+// // #include <bits/stdc++.h>
+// // using namespace std;
+
+// // int main() {
+// //     // Debugging DSA code
+// //     int row, column;
+    
+// //     for (row = 1; row <= 5; row++) {
+// //         for (column = 1; column <= 5; column++) {
+// //             cout << "*" << " ";
+// //         }
+// //         cout << endl;
+// //     }
+
+// // }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main (){
+//   int row, column;
+
+//   for (row = 1; row <= 5; row = row + 1){
+//     for (column = 1; column <= 5; column = column+1){
+//       cout << row << " ";
+//     }
+//     cout<<endl;
+//   }
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int row, column;
+
+//     for (row = 1; row <= 5; row = row + 1)
+//     {
+//       for (column = 5; column >=1; column = column - 1)
+//       {
+//         cout << column << " ";
+//       }
+//       cout << endl;
+//     }
+    
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int row, column;
+
+//     for (row = 1; row <= 5; row = row + 1)
+//     {
+//       char name = 'a' + row -1;
+//       for (column = 1; column <=5; column = column + 1)
+//       {
+//         cout <<name<< " ";
+//       }
+//       cout << endl;
+//     }
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     // Your DSA code here
+//     int row, column;
+
+//     for (row = 1; row <=5; row = row + 1)
+//     {
+//       for(column = 'a'; column <= 'e'; column = column + 1){
+//         cout << char(column) << " ";
+//       }
+//       cout << endl;
+//     }
+// }
