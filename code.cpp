c80f95afe442e24ff37187858caa275bcc9259e3
Abhilash <abhilashvishwa12@gmail.com>
2025-03-18 13:27:09 +0530

    introduction to array

--- code.cpp
+++ code.cpp
@@ -347,15 +347,33 @@
 //   }
 //  }
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
-  int n;
-  cin >> n;
-    for (int i = 0; i <= n; i = i + 1 ){
-      cout << i*i << endl;
-    }
+// int main() {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= 10; i = i + 1) {
+//         cout << n*i << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int arr[6] = {1, 2, 3, 4, 5, 6};
+//     int ans = INT_MAX;
+
+//     for (int i = 0; i < 5; i++) {
+//         if (arr[i] < ans) {
+//             ans = arr[i];
+//         }
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
+
